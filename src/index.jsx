4f14266d434ba0678446e9059a094f0fafe3cb5a
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-09 21:58:35 +0900

    added bootstrap and loaders

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "./main.scss";
+
+const root = document.querySelector('#root');
+ReactDOM.render(<App />, root);
